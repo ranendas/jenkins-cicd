aa6f958b61f34a2e0500a7bde06e333d6424f1b5
Ranendra Das <rndas@dtcc.com>
2025-06-30 17:51:45 -0400

    First commit

--- src/main/java/com/test/App.java
+++ src/main/java/com/test/App.java
@@ -19,6 +19,8 @@ public class App {
         return "CI/CD Application Deployed successfully in ";
     }
 
+
+
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
